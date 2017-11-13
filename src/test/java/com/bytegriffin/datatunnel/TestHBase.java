5c8341afb971f980f1ff276fbc7051781a6b0ff4
bytegriffin <bytegriffin@126.com>
2017-11-13 20:28:40 +0800

    增加测试代码

---
+++ src/test/java/com/bytegriffin/datatunnel/TestHBase.java
@@ -0,0 +1,120 @@
+package com.bytegriffin.datatunnel;
+
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.HColumnDescriptor;
+import org.apache.hadoop.hbase.HTableDescriptor;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.Admin;
+import org.apache.hadoop.hbase.client.Connection;
+import org.apache.hadoop.hbase.client.ConnectionFactory;
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.client.ResultScanner;
+import org.apache.hadoop.hbase.client.Scan;
+import org.apache.hadoop.hbase.client.Table;
+import org.apache.hadoop.hbase.util.Bytes;
+
+public class TestHBase {
+	
+	private static String zkaddress = "localhost:2181";
+	private static String tablename = "t1";
+	private static String colomnfamily = "cf";
+
+	/**
+	 * 获取链接
+	 * @return
+	 */
+	public static Connection getConnection() {
+		Configuration configuration = HBaseConfiguration.create();
+		configuration.set("hbase.zookeeper.quorum", zkaddress);
+		Connection connection = null;
+		try {
+			connection = ConnectionFactory.createConnection(configuration);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return connection;
+	}
+	
+	/**
+	 * 创建表
+	 * create 'table_name','column_family'
+	 */
+	public static void createTable() {
+		Connection conn = getConnection();
+        TableName tableName = TableName.valueOf(tablename);
+        Admin admin = null;
+		try {
+			admin = conn.getAdmin();
+			if(admin.tableExists(tableName)){
+				admin.disableTable(tableName);  
+				admin.deleteTable(tableName);
+				System.out.println("表已经存在，先删除");
+	        } 
+            HTableDescriptor hTableDescriptor = new HTableDescriptor(tableName);
+            HColumnDescriptor family= new HColumnDescriptor(colomnfamily);
+            hTableDescriptor.addFamily(family);
+            admin.createTable(hTableDescriptor);
+            System.out.println("表创建成功");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * 插入数据
+	 * put <table>,<rowkey>,<family:column>,<value>
+	 */
+	public static void insertData() {
+		Connection conn = getConnection();
+		try {
+			Table table = conn.getTable(TableName.valueOf(tablename));
+			Put put = new Put(Bytes.toBytes("row"));
+			put.addColumn(Bytes.toBytes(colomnfamily), Bytes.toBytes("column1"), Bytes.toBytes("123123123"));
+			table.put(put);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * 查询表
+	 * get <table>,<rowkey>,[<family:column>,....]
+	 */
+	public static void query() {
+		Connection conn = getConnection();
+		try {
+			Table table = conn.getTable(TableName.valueOf(tablename));
+			ResultScanner scanner = table.getScanner(new Scan());
+			scanner.forEach(result -> {
+				System.out.println("rowkey="+ new String(result.getRow()));
+				result.listCells().forEach(cell -> {
+					String columnFamily = Bytes.toString(cell.getFamilyArray(), cell.getFamilyOffset(), cell.getFamilyLength());
+					String columnName = Bytes.toString(cell.getQualifierArray(), cell.getQualifierOffset(), cell.getQualifierLength());
+					String columnValue = Bytes.toString(cell.getValueArray(), cell.getValueOffset(), cell.getValueLength());
+					System.out.println("colomnfamily="+columnFamily+" qualifier="+columnName+" value="+columnValue);
+				});
+			});
+
+//	        Get get = new Get("row".getBytes());
+//	        Result result = table.get(get);
+//	        System.out.println("row key is:" + new String(result.getRow()));
+
+//	        result.listCells().forEach(cell -> {
+//	        	System.out.println("colomnfamily="+new String(cell.getFamilyArray()) 
+//	        			+ "qualifier=" + new String(cell.getQualifierArray())
+//						+ "value=" + new String(cell.getValueArray()));
+//	        });
+	        
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		query();
+	}
+
+}
--- src/test/java/com/bytegriffin/datatunnel/TestKafka.java
+++ src/test/java/com/bytegriffin/datatunnel/TestKafka.java
@@ -1,30 +1,76 @@
 package com.bytegriffin.datatunnel;
 
+import java.util.Arrays;
 import java.util.Properties;
 
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringDeserializer;
 
+/**
+ * 1.启动内置zookeeper
+ * ./bin/zookeeper-server-start.sh -daemon ./config/zookeeper.properties
+ * 2.启动kafka
+ * ./bin/kafka-server-start.sh -daemon ./config/server.properties
+ * 3.创建一个名为"test"的topic
+ * ./bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic test
+ * 4.生产消息
+ * ./bin/kafka-console-producer.sh --broker-list localhost:9092 --topic test
+ * 5.消费消息
+ * ./bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic test --from-beginning
+ */
 public class TestKafka {
 
-	public static void main(String[] args) {
+	private static String topic = "kafka_topic";
+
+	private static void produce() {
 		Properties props = new Properties();
-		props.put("bootstrap.servers", "localhost:9092");
-		props.put("acks", "all");
-		props.put("retries", 0);
-		props.put("batch.size", 16384);
-		props.put("linger.ms", 1);
-		props.put("buffer.memory", 33554432);
-		props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-		props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+		props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,  "localhost:9092");
+		props.put(ProducerConfig.ACKS_CONFIG, "all");
+		props.put(ProducerConfig.RETRIES_CONFIG, 0);
+		props.put(ProducerConfig.BATCH_SIZE_CONFIG, 16384);
+		props.put(ProducerConfig.LINGER_MS_CONFIG, 1);
+		props.put(ProducerConfig.BUFFER_MEMORY_CONFIG, 33554432);
+		props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+		props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
 
 		Producer<String, String> producer = new KafkaProducer<>(props);
-		for (int i = 0; i < 100; i++){
-			producer.send(new ProducerRecord<>("topic1", Integer.toString(i), Integer.toString(i)));
-			System.out.println("send成功");
+		for (int i = 0; i < 10; i++){
+			producer.send(new ProducerRecord<>(topic, Integer.toString(i), Integer.toString(i)));
+			System.out.println("生产成功："+Integer.toString(i));
 		}
-		
+
 		producer.close();
 	}
+
+	private static void consume() {
+		Properties props = new Properties();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG ,"test") ;
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true");
+        props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+
+        @SuppressWarnings("resource")
+		Consumer<String, String> consumer = new KafkaConsumer<>(props);
+        consumer.subscribe(Arrays.asList(topic));
+
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(100);
+            records.forEach(record -> System.out.println("消费成功："+record.value()));
+        }
+	}
+
+	public static void main(String[] args) {
+		produce();
+		consume();
+	}
+
 }
